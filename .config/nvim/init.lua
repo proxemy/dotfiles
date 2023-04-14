ee2797a74d0748ac6b30154e22320169dbcc334f
Proxemy <33573115+proxemy@users.noreply.github.com>
2023-04-14 11:47:25 +0200

    Moved nvim/init.vim to init.lua and included lua syntax for nvim to load properly.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,4 @@
+vim.cmd( [[
 
 " disable nvims cursor reshaping
 let $NVIM_TUI_ENABLE_CURSOR_SHAPE = 0
@@ -7,3 +8,5 @@ set guicursor=
 set runtimepath^=~/.vim runtimepath+=~/.vim/after
 let &packpath = &runtimepath
 source ~/.vimrc
+
+]] )
