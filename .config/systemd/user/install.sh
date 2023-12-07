ac823f4bf1b1bd3fcab0fbce733fa17edc719dba
Proxemy <33573115+proxemy@users.noreply.github.com>
2023-12-07 12:55:36 +0100

    Final fix. Systemd is not mature.

--- .config/systemd/user/install.sh
+++ .config/systemd/user/install.sh
@@ -2,17 +2,23 @@
 
 set -eux
 
+sudo find /etc/systemd -iname 'steam-guard.*' -delete
+sudo find /lib/systemd -iname 'steam-guard.*' -delete
+
 UNITS=( $HOME/.config/systemd/user/*.{service,timer} )
 
-for u in ${UNITS[@]}; do
-	systemd-analyze verify "$u"
-	sudo systemctl stop $(basename "$u") || true
-	sudo systemctl disable $(basename "$u") || true
-	sudo systemctl enable $(realpath "$u")
-	#sudo systemctl start $(basename "$u")
-done
+systemd-analyze verify ${UNITS[@]}
 
-#sudo systemctl daemon-reload
+sudo chown root:root ${UNITS[@]}
+sudo chmod u+rw-x,go+r-wx ${UNITS[@]}
+
+# it appears systemd does not allow user unit installed system wide, thus copy
+sudo cp ${UNITS[@]} /lib/systemd/system
+
+sudo systemctl enable --system --now steam-guard.service
+sudo systemctl enable --system --now steam-guard.timer
+
+sudo systemctl daemon-reload
 
 systemctl is-enabled steam-guard.timer
 systemctl is-active steam-guard.timer
