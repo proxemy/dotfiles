29f2d47d42aa8d964d810b4a9f58784336703d19
Proxemy <33573115+proxemy@users.noreply.github.com>
2024-07-04 20:24:52 +0200

    Finalized rnd_manager and auxiliary script.

--- scripts/rnd_manager.sh
+++ scripts/rnd_manager.sh
@@ -2,45 +2,44 @@
 
 set -euo pipefail
 
+
+on_exit()
+{
+	echo "Done"
+	echo -e "\x44\x4f\x4e\x54\x20\x46\x4f\x52\x47\x45\x54\x20\x54\x4f\x20\x43\x4c\x45\x41\x52\x20\x48\x49\x53\x54\x4f\x52\x59 \x57\x49\x54\x48"
+	echo -e "\x2\x73\x6f\x75\x72\x63\x65\x20\x63\x6c\x65\x61\x72\x5f\x68\x69\x73\x74\x6f\x72\x79\x2e\x73\x68"
+}
+trap on_exit EXIT ERR
+
+
 echo Start
 
-#TMP="${TMP:-/dev/shm}"
 SOURCE="${1:-'\$1'}"
 TARGET="${2:-'\$2'}"
 OFFSET=0
 SIZE="${SIZE:-0}"
 
 
-#test -d "$TMP" || { echo "Temp $TMP directory not found"; exit 1; }
 test -f "$SOURCE" || { echo "SOURCE (\$1) file not found"; exit 1; }
 test -f "$TARGET" || { echo "TARGET (\$2) file not found"; exit 1; }
 test "$SIZE" -ne 0 || { echo "\$SIZE cannot be zero/undefined"; exit 1; }
 [ "$SOURCE" != "$TARGET" ] || { echo "Target and source file cannot be identical"; exit 1; }
 
 
-#TMP=$(mktemp -dp "$TMP")
-#on_exit() {
-	#echo Done;
-	#rm -rf "$TMP"
-	#history -d 1
-#}
-#trap on_exit EXIT ERR
-
-
 echo -n "Analyzing source file "
 hex_dump=$(xxd -p "$SOURCE" | tr -d '\n')
 for i in $(seq 0 500); do
 	echo -n "."
-	sha1=$(head --byte=+"$i" "$SOURCE" | sha256sum -b | cut -f1 -d' ')
-	if [[ "$hex_dump" =~ "$sha1" ]]; then
+	sha=$(head --byte=+"$i" "$SOURCE" | sha256sum -b | cut -f1 -d' ')
+	if [[ "$hex_dump" =~ "$sha" ]]; then
 		OFFSET=$(($i + 32))
+		echo -e "\nFound hash at '$i': $sha"
 		break
 	fi
 done
-echo
 
 if [[ $OFFSET -eq 0 ]]; then
-	echo Could not find offest in target file "$TARGET".
+	echo -e "\nCould not dertermine offest in target file: $TARGET".
 	exit 1
 fi
 
@@ -51,14 +50,12 @@ if [[ $SIZE -gt 0 ]]; then
 	{
 		head --bytes="$OFFSET" "$SOURCE";
 		head --bytes="$SIZE" < /dev/urandom;
-		tail --bytes=+$(("$SIZE" + 1)) "$SOURCE";
+		tail --bytes=+$(("$OFFSET" + 1)) "$SOURCE";
 	} > "$TARGET"
 elif [[ $SIZE -lt 0 ]]; then
 	echo Removing ...
 	{
-		head --bytes=$(( "$OFFSET" * -1 )) "$SOURCE";
-		tail --bytes=+$(( "$SIZE" * -1 + 1 )) "$SOURCE"
+		head --bytes=$(( "$OFFSET" )) "$SOURCE";
+		tail --bytes=+$(( "$OFFSET" + ("$SIZE" * -1) + 1 )) "$SOURCE"
 	} > "$TARGET"
 fi
-
-echo Done
