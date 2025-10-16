9d7774eae16faec2d83457ad51894048492ebe59
Proxemy <33573115+proxemy@users.noreply.github.com>
2025-10-16 18:03:11 +0200

    Aded safety check to not repeatedly pad identical files.

--- scripts/rnd_manager.sh
+++ scripts/rnd_manager.sh
@@ -5,8 +5,15 @@ set -euo pipefail
 
 SOURCE="${1:-$ARG1_SOURCE_FILE}"
 TARGET="${2:-$ARG2_TARGET_FILE}"
+TMP_TARGET=$(mktemp "$TARGET".XXX)
 OFFSET=0
 SIZE=0
+
+
+on_exit() {
+	test -f "$TMP_TARGET" && rm "$TMP_TARGET"
+}
+trap on_exit ERR EXIT
 
 
 test -f "$SOURCE" || { echo "SOURCE (\$1) file not found"; exit 1; }
@@ -14,10 +21,15 @@ test -f "$SOURCE" || { echo "SOURCE (\$1) file not found"; exit 1; }
 read -r -p "Please enter size: " SIZE
 test "$SIZE" -ne 0 || { echo "\$SIZE cannot be zero/undefined"; exit 1; }
 
+SOURCE_SIZE=$(du -b "$SOURCE" | cut -f1)
+if [ $(($SIZE * 2)) -ge $SOURCE_SIZE ]; then
+	echo "\$SIZE cannot be larger then 1/2 of source file size in bytes: $SOURCE_SIZE" >&2
+	exit 1
+fi
+
 if [ -f "$TARGET" ]; then
-	echo
-	read -p "Target file '$TARGET' already exists! Overwrite? [y/N]" inp
-	if [[ ! "y|Y" =~ "$inp" ]]; then
+	read -p "Target file '$TARGET' already exists! Overwrite? [y/N]"
+	if [[ ! "y|Y" =~ $REPLY ]]; then
 		echo Aborting
 		exit
 	fi
@@ -51,11 +63,24 @@ if [[ $SIZE -gt 0 ]]; then
 		head --bytes="$OFFSET" "$SOURCE";
 		head --bytes="$SIZE" < /dev/urandom;
 		tail --bytes=+$(("$OFFSET" + 1)) "$SOURCE";
-	} > "$TARGET"
+	} > "$TMP_TARGET"
 elif [[ $SIZE -lt 0 ]]; then
 	echo "Removing ... $SIZE bytes"
 	{
 		head --bytes=$(( "$OFFSET" )) "$SOURCE";
 		tail --bytes=+$(( "$OFFSET" + ("$SIZE" * -1) + 1 )) "$SOURCE"
-	} > "$TARGET"
+	} > "$TMP_TARGET"
 fi
+
+if [[ -f "$TARGET" ]] && [[ $SIZE -gt 0 ]]; then
+	TARGET_TAIL_HASH=$(tail --bytes=50 "$TARGET" | md5sum | cut -d' ' -f1)
+	TMP_TARGET_TAIL_HASH=$(tail --bytes=50 "$TMP_TARGET" | md5sum | cut -d' ' -f1)
+
+	if [[ "$TARGET_TAIL_HASH" == "$TMP_TARGET_TAIL_HASH" ]]; then
+		echo ERROR: You should not add random bytes to identical source/target files. >&2
+		echo It would reveal the secret offset. Aborting! >&2
+		exit 1
+	fi
+fi
+
+cp "$TMP_TARGET" "$TARGET"
